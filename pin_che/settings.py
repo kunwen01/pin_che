e4e024a90711c7d120b3362ef8e345939839c8f0
wangkun <kunwen01@outlook.com>
2019-12-23 05:11:31 +0800

    静态文件

--- pin_che/settings.py
+++ pin_che/settings.py
@@ -132,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # python manage.py crontab add， show ， remove
 CRONJOBS = [
--- pin_che/urls.py
+++ pin_che/urls.py
@@ -18,12 +18,14 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from fontapp.views import index, CarShareViewSet, ThirdPartyViewSet
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('', index)
-]
+] + static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
 router = DefaultRouter()
 router.register(r'carshare', CarShareViewSet)
 router.register(r'thirdpart', ThirdPartyViewSet)
