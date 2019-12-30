5a6a31b7d4115f4fbc175cc2489a489f00470f30
wangkun <kunwen01@outlook.com>
2019-12-31 06:59:29 +0800

    test2

--- pin_che/settings.py
+++ pin_che/settings.py
@@ -136,6 +136,6 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # python manage.py crontab add， show ， remove
 CRONJOBS = [
-    ('59 6 * * *', 'fontapp.cron.thirdpartyinfo', '>>./thirdpartyinfo.log'),
+    ('0 7 * * *', 'fontapp.cron.thirdpartyinfo', '>>./thirdpartyinfo.log'),
     # ('*/5 * * * *', 'fontapp.cron.test', '>>./test.log'),
 ]
